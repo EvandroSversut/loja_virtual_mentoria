9c13ad81da75eef31b05055704d4f73d0b0aad6b
Lenovo <Lenovo@LAPTOP-IN00I31T>
2023-12-09 18:08:48 -0300

    classe imagem produto

--- src/main/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplication.java
+++ src/main/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// @EntityScan(basePackages = "jdev.mentoria.lojavirtual.model")
+// se nao tiver criando as tabelas no banco, usar esta anotacao
 public class LojaVirtualMentoriaApplication {
 
 	public static void main(String[] args) {
