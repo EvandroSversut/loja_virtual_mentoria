6d25e2fa0508ecc0eee0281441a0b0ecb1df8b76
Lenovo <Lenovo@LAPTOP-IN00I31T>
2023-12-09 07:41:44 -0300

    implmentacao classes de model

---
+++ src/main/java/jdev/mentoria/lojavirtual/model/PessoaJuridica.java
@@ -0,0 +1,75 @@
+package jdev.mentoria.lojavirtual.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name= "pessoa_juridica")
+@PrimaryKeyJoinColumn(name = "id")
+public class PessoaJuridica extends Pessoa {
+
+	private static final long serialVersionUID = 1L;
+
+	private String cnpj;
+	
+	private String inscEst;
+	
+	private String inscMun;
+	
+	private String nomeFant;
+	
+	private String razaoSocial;
+	
+	private String categoria;
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public String getInscEst() {
+		return inscEst;
+	}
+
+	public void setInscEst(String inscEst) {
+		this.inscEst = inscEst;
+	}
+
+	public String getInscMun() {
+		return inscMun;
+	}
+
+	public void setInscMun(String inscMun) {
+		this.inscMun = inscMun;
+	}
+
+	public String getNomeFant() {
+		return nomeFant;
+	}
+
+	public void setNomeFant(String nomeFant) {
+		this.nomeFant = nomeFant;
+	}
+
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+}
