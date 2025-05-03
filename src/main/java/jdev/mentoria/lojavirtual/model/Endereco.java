af2836826682249eae9f34d57946cfd66224b5e7
Evandro <svecontabilidade@gmail.com>
2025-05-03 07:59:50 -0300

    Criada as Enum, Usuario e Usuario_acesso

--- src/main/java/jdev/mentoria/lojavirtual/model/Endereco.java
+++ src/main/java/jdev/mentoria/lojavirtual/model/Endereco.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 import jakarta.persistence.ConstraintMode;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,6 +14,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
+import jdev.mentoria.lojavirtual.enums.TipoEndereco;
 
 @Entity
 @Table( name = "endereco")
@@ -40,6 +43,9 @@ public class Endereco implements Serializable {
    @JoinColumn(name = "pessoa_id", nullable = false, 
    foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
    private Pessoa pessoa;
+
+   @Enumerated(EnumType.STRING)
+   private TipoEndereco tipoEndereco;
 
    public Long getId() {
     return id;
@@ -111,6 +117,14 @@ public class Endereco implements Serializable {
 
    public void setPessoa(Pessoa pessoa) {
     this.pessoa = pessoa;
+   }
+
+   public TipoEndereco getTipoEndereco() {
+       return tipoEndereco;
+   }
+
+   public void setTipoEndereco(TipoEndereco tipoEndereco) {
+       this.tipoEndereco = tipoEndereco;
    }
 
    @Override
